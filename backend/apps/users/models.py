b6f051bf71d3d7561153e1f3921f1eb36ea45139
Maihos <monteirojuninho@hotmail.com>
2025-08-07 00:01:26 -0300

    Refatora modelo Usuario para incluir campos de telefone e lógica de autenticação

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+from apps.users.service import UsuarioService
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    numero_telefone = models.CharField(max_length=20)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UsuarioService()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome', 'numero_telefone']
+
+    def __str__(self):
+        return self.email
