b8ad50f17b8e7d37d9864196b1f734e23c38358d
rafaelgoesti <rafaelgoesti2021@gmail.com>
2025-08-09 11:19:55 -0300

    Service e ViewSet implementada com sucesso!

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('apps.custom_orders.urls')),
+    path('api/', include('apps.orders.urls')),
 ]
