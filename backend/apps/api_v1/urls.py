9678fe10b38bc2e8c35abbefa985d83ed10c2c3c
Maihos <monteirojuninho@hotmail.com>
2025-08-21 22:46:35 -0300

    Refatora rotas para usuários e avaliações; adiciona novo arquivo de roteamento para reviews

--- backend/apps/api_v1/urls.py
+++ backend/apps/api_v1/urls.py
@@ -7,17 +7,43 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from apps.users import views as users_views
 from apps.custom_orders import views as custom_orders_views
+from apps.reviews import views as reviews_views
 
 
 router = DefaultRouter()
 
-router.register(r'usuarios', users_views.UsuarioView, basename='usuario')
-router.register(r'custom-orders', custom_orders_views.CustomOrderViewSet, basename='custom-order')
+router.register(
+    r'usuarios',
+    users_views.UsuarioView,
+    basename='usuario'
+)
+router.register(
+    r'custom-orders',
+    custom_orders_views.CustomOrderViewSet,
+    basename='custom-order'
+)
+router.register(
+    r'reviews',
+    reviews_views.ReviewView,
+    basename='review'
+)
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path('', include(router.urls)),  # Adiciona as rotas do router
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema"
+    ),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    ),
+    path('', include(router.urls)),
 ]
