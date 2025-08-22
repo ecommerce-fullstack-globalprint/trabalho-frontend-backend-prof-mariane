c352371d7a3b1e7938e3960e86841d3f266bc387
Maihos <monteirojuninho@hotmail.com>
2025-08-21 22:05:32 -0300

    Adiciona arquivo .env.example com configurações do Django e PostgreSQL; implementa ReviewSerializer e ReviewView para gerenciamento de avaliações

--- backend/apps/reviews/views.py
+++ backend/apps/reviews/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from . import models, serializers
+
 
 # Create your views here.
+class ReviewView(viewsets.ModelViewSet):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        serializer = super().get_serializer(*args, **kwargs)
+        # You can add any additional modifications to the serializer here
+        return serializer
