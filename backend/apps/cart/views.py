68fe2a0289e21034fd917659bd4eaf59b0138a6e
AlbertoMJr0 <monteirojuninho@hotmail.com>
2025-10-09 16:09:40 -0300

    feat: implementa CartViewSet com suporte a autenticação e serialização

--- backend/apps/cart/views.py
+++ backend/apps/cart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Cart
+from .serializers import CartSerializer, CartListSerializer
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CartSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return CartListSerializer
+        return CartSerializer
 
 # Create your views here.
