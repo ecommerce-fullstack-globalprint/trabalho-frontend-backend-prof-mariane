cc02ac3dfd2b4aabdeabef1e107d50f167abde41
AlbertoMJr0 <monteirojuninho@hotmail.com>
2025-10-09 16:09:14 -0300

    fix: melhora a formatação do registro do roteador para maior legibilidade

--- backend/apps/custom_orders/routers.py
+++ backend/apps/custom_orders/routers.py
@@ -3,5 +3,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'custom-orders', views.CustomOrderViewSet, basename='custom-order')
-
+router.register(
+    r'custom-orders',
+    views.CustomOrderViewSet,
+    basename='custom-order'
+)
